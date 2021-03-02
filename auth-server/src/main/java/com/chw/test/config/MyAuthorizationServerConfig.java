9001b71a3b59fd1d737582a01eb8a09f3f11db33
ChenWei <545764502@qq.com>
2021-03-02 11:12:57 +0800

    oauth2重温

--- auth-server/src/main/java/com/chw/test/config/MyAuthorizationServerConfig.java
+++ auth-server/src/main/java/com/chw/test/config/MyAuthorizationServerConfig.java
@@ -1,6 +1,7 @@
 package com.chw.test.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
@@ -8,6 +9,8 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.A
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
 
 import javax.sql.DataSource;
 
@@ -30,38 +33,48 @@ public class MyAuthorizationServerConfig extends AuthorizationServerConfigurerAd
         super.configure(security);
     }
 
-    /**
-     * 该处通过配置ClientDetailsService来配置注册到该授权服务器的clients信息
-     * 注意：除非下面的configure(AuthorizationServerEndpointsConfigurer endpoints)指定了一个authenticationManager，否则密码授权模式不可用
-     * 至少要配制一个client，否则服务器无法启动
-     */
+//    /**
+//     * 该处通过配置ClientDetailsService来配置注册到该授权服务器的clients信息
+//     * 注意：除非下面的configure(AuthorizationServerEndpointsConfigurer endpoints)指定了一个authenticationManager，否则密码授权模式不可用
+//     * 至少要配制一个client，否则服务器无法启动
+//     */
+//    @Override
+//    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+//        clients.inMemory()
+//                    //client-id
+//                    .withClient("client-for-server")
+//                    //client-secret
+//                    .secret(passwordEncoder.encode("client-for-server"))
+//                    //该client支持的授权模式
+//                    //oauth的client在请求code时，只有传递授权模式参数，该处包含的授权模式才可以访问
+//                    //可选值：authorization_code、password、refresh_token、implicit和client_credentials
+//                    .authorizedGrantTypes("authorization_code","implicit")
+//                    //该client分配的access_token的有效时间要少于刷新时间
+//                    .accessTokenValiditySeconds(7200)
+//                    //该client分配的access_token的可刷新时间要多于有效时间
+//                    //超过有效时间，但在可刷新时间范围内的access_token也可以刷新
+//                    .refreshTokenValiditySeconds(72000)
+//                    //重定向URL，这是客户端注册时，客户端提供的
+//                    .redirectUris("http://localhost:3333/login/oauth2/code/chw-auth")
+//                    .additionalInformation()
+//                    //该client可以访问的资源服务器的id
+//                    .resourceIds(MyResourceServerConfig.RESOURCES_ID)
+//                    //该client拥有的权限，资源服务器可以依据该处定义的范围对client进行鉴权
+//                    .authorities("ROLE_CLIENT")
+//                    //自动批准的范围（scope），自动批准的scope在批准页不需要显示，即不需要用户确认批准
+//                    //如果所有scope都自动批准，则不显示批准页
+//                    .scopes("profile","email","phone")
+//                    .autoApprove("profile");
+//    }
+
     @Override
     public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
-        clients.inMemory()
-                    //client-id
-                    .withClient("client-for-server")
-                    //client-secret
-                    .secret(passwordEncoder.encode("client-for-server"))
-                    //该client支持的授权模式
-                    //oauth的client在请求code时，只有传递授权模式参数，该处包含的授权模式才可以访问
-                    //可选值：authorization_code、password、refresh_token、implicit和client_credentials
-                    .authorizedGrantTypes("authorization_code","implicit")
-                    //该client分配的access_token的有效时间要少于刷新时间
-                    .accessTokenValiditySeconds(7200)
-                    //该client分配的access_token的可刷新时间要多于有效时间
-                    //超过有效时间，但在可刷新时间范围内的access_token也可以刷新
-                    .refreshTokenValiditySeconds(72000)
-                    //重定向URL，这是客户端注册时，客户端提供的
-                    .redirectUris("http://localhost:3333/login/oauth2/code/chw-auth")
-                    .additionalInformation()
-                    //该client可以访问的资源服务器的id
-                    .resourceIds(MyResourceServerConfig.RESOURCES_ID)
-                    //该client拥有的权限，资源服务器可以依据该处定义的范围对client进行鉴权
-                    .authorities("ROLE_CLIENT")
-                    //自动批准的范围（scope），自动批准的scope在批准页不需要显示，即不需要用户确认批准
-                    //如果所有scope都自动批准，则不显示批准页
-                    .scopes("profile","email","phone")
-                    .autoApprove("profile");
+        clients.jdbc(dataSource);
+    }
+
+    @Bean
+    public TokenStore jdbcTokenStore(){
+        return new JdbcTokenStore(dataSource);
     }
 
     /**
@@ -71,5 +84,6 @@ public class MyAuthorizationServerConfig extends AuthorizationServerConfigurerAd
     @Override
     public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
         super.configure(endpoints);
+        endpoints.tokenStore(jdbcTokenStore());
     }
 }
